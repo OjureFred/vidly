07ff05db089c87479fdac5f2deb1186bfd924f76
Fredrick Ojure <fredojure@gmail.com>
2022-08-29 10:16:42 +0300

    2. Created the like component of the movie and added event to handle clicking

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Like from './common/like'
+
 import { getMovies } from '../services/fakeMovieService';
 
 class Movies extends Component {
@@ -8,11 +10,20 @@ class Movies extends Component {
      };
 
      handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies})
         console.log(movie)
-     }
+     };
 
     render() {
-        return <table className="table">
+        const { length: count} = this.state.movies;
+        if (this.state.movies.length ===0)
+            return <p>There are no movies in the database </p>
+        
+        return (
+        <div>
+        <p>Showing { count } movies in the database</p>
+        <table className="table table-striped">
             <thead>
                 <tr>
                     <th>Title</th>
@@ -20,21 +31,31 @@ class Movies extends Component {
                     <th>Stock</th>
                     <th>Rate</th>
                     <th></th>
+                    <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 {this.state.movies.map( movie => (
-                     <tr>
+                     <tr key={ movie._id}>
                     <td>{movie.title}</td>
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><Like liked = {movie.liked} onClick={this.handleLike} /></td>
+                    <td></td>
                     <td><button onClick = {() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>
                 ))}
                
             </tbody>
         </table>
+        </div>
+        )
+    }
+
+    handleLike = () => {
+        console.log('Like clicked')
     }
 
 }
