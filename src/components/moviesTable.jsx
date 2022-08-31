29cbb49a5aabf13b760c8aaab91a0d9b5ea7eeb3
Fredrick Ojure <fredojure@gmail.com>
2022-08-31 12:13:03 +0300

    12. Refactored out MoviesTable to be a reusable component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,19 +1,31 @@
-import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import Like from './common/like';
 
+class MoviesTable extends Component {
 
-const MoviesTable = (props) => {
-    const {movies, onDelete, onLike, onSort} = props;
-    return ( 
-        
+    raiseSort = path => {
+        const sortColumn = {...this.props.sortColumn};
+        if (sortColumn.path === path)
+            sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
+        else {
+            sortColumn.path = path;
+            sortColumn.order = 'asc'
+        }
+        this.props.onSort(sortColumn);
+     };
+     
+    render() { 
+         const {movies, onDelete, onLike} = this.props;
+
+         return(
+
         <table className="table table-striped">
         <thead>
             <tr>
-                <th onClick = {()=> onSort("title")}>Title</th>
-                <th onClick = {()=> onSort("genre.name")}>Genre</th>
-                <th onClick = {()=> onSort("numberInStock")}>Stock</th>
-                <th onClick = {()=> onSort("dailyRentalRate")}>Rate</th>
+                <th onClick = {()=> this.raiseSort("title")}>Title</th>
+                <th onClick = {()=> this.raiseSort("genre.name")}>Genre</th>
+                <th onClick = {()=> this.raiseSort("numberInStock")}>Stock</th>
+                <th onClick = {()=> this.raiseSort("dailyRentalRate")}>Rate</th>
                 <th></th>
                 <th></th>
             </tr>
@@ -33,7 +45,11 @@ const MoviesTable = (props) => {
         </tbody>
         
     </table>
-    );
+         );
+        
+    }
 }
+ 
+
  
 export default MoviesTable;
